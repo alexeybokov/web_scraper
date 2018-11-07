f745b310ea4f6886fddadf5fd9fa9786b10d5ae7
alexeybokov <lexabokov18@gmail.com>
2018-11-07 15:40:07 +0200

    refactor sql query

--- app/lib/sql_formatter.rb
+++ app/lib/sql_formatter.rb
@@ -33,12 +33,31 @@ class SqlFormatter
 
   def self.query_for_receipts_revenues(parser, ilearn_districts_id)
     %{
-      INSERT INTO receipts_revenues (district_number, district_name, state_amount, local_amount,
-              federal_amount, total_amount, data_source_url, scrape_dev, ilearn_districts_id)
+      INSERT INTO receipts_revenues
+        (
+          district_number,
+          district_name,
+          state_amount,
+          local_amount,
+          federal_amount,
+          total_amount,
+          data_source_url,
+          scrape_dev,
+          ilearn_districts_id
+        )
 
-      VALUES ('#{parser.number}', '#{parser.name}', #{parser.state_amount}, #{parser.local_amount},
-              #{parser.federal_amount}, #{parser.total_amount}, '#{parser.data_source_url}',
-             '#{parser.scrape_dev}', #{ilearn_districts_id});
+      VALUES
+        (
+          '#{parser.number}',
+          '#{parser.name}',
+          #{parser.state_amount},
+          #{parser.local_amount},
+          #{parser.federal_amount},
+          #{parser.total_amount},
+          '#{parser.data_source_url}',
+          '#{parser.scrape_dev}',
+          #{ilearn_districts_id}
+        );
     }
   end
 
